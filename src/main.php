76483849d401b9b5effcfa56403050409a71ea9e
Rndoom04 <53907816+Rndoom04@users.noreply.github.com>
2022-03-02 15:07:18 +0100

    setAlgorithm method

--- src/main.php
+++ src/main.php
@@ -13,6 +13,70 @@
         
         /* Encryption and decryption method - do not change! */
         private $encryption_method = "AES-128-CBC";
+            
+        /* Allowed algorithms */
+        private $allowed_algorithms = [
+            "AES-128-CBC",
+            "AES-128-CBC-HMAC-SHA1",
+            "AES-128-CFB",
+            "AES-128-CFB1",
+            "AES-128-CFB8",
+            "AES-128-CTR",
+            "AES-128-OFB",
+            "AES-192-CBC",
+            "AES-192-CFB",
+            "AES-192-CFB1",
+            "AES-192-CFB8",
+            "AES-192-CTR",
+            "AES-192-OFB",
+            "AES-256-CBC",
+            "AES-256-CBC-HMAC-SHA1",
+            "AES-256-CFB",
+            "AES-256-CFB1",
+            "AES-256-CFB8",
+            "AES-256-CTR",
+            "AES-256-OFB",
+            "BF-CBC",
+            "BF-CFB",
+            "BF-OFB",
+            "CAMELLIA-128-CBC",
+            "CAMELLIA-128-CFB",
+            "CAMELLIA-128-CFB1",
+            "CAMELLIA-128-CFB8",
+            "CAMELLIA-128-OFB",
+            "CAMELLIA-192-CBC",
+            "CAMELLIA-192-CFB",
+            "CAMELLIA-192-CFB1",
+            "CAMELLIA-192-CFB8",
+            "CAMELLIA-192-OFB",
+            "CAMELLIA-256-CBC",
+            "CAMELLIA-256-CFB",
+            "CAMELLIA-256-CFB1",
+            "CAMELLIA-256-CFB8",
+            "CAMELLIA-256-OFB",
+            "CAST5-CBC",
+            "CAST5-CFB",
+            "CAST5-OFB",
+            "DES-CBC",
+            "DES-CFB",
+            "DES-CFB1",
+            "DES-CFB8",
+            "DES-EDE-CBC",
+            "DES-EDE-CFB",
+            "DES-EDE-OFB",
+            "DES-EDE3-CBC",
+            "DES-EDE3-CFB",
+            "DES-EDE3-CFB1",
+            "DES-EDE3-CFB8",
+            "DES-EDE3-OFB",
+            "DES-OFB",
+            "DESX-CBC",
+            "RC2-40-CBC",
+            "RC2-64-CBC",
+            "RC2-CBC",
+            "RC2-CFB",
+            "RC2-OFB"
+        ];
         
         
         /**
@@ -39,6 +103,33 @@
             }
             
             return false;
+        }
+        
+        /**
+         * Set global algorithm method to encrypt and decrypt
+         * @param string $method
+         * @return bool
+        */
+        public function setAlgorithm($method) {
+            if (!empty($method)) {
+                // Is in allowed methods?
+                if (in_array($method, $this->encryption_method)) {
+                    $this->encryption_method = $method;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            
+            return false;
+        }
+        
+        /**
+         * Get allowed supported algorithms methods
+         * @return array
+        */
+        public function getAlgorithms() {
+            return $this->encryption_method;
         }
 
         /**
